6aa81e06045edbed16ac7a11b932f76159cb2d3e
shinstah <wgs2014@att.net>
2015-12-09 05:21:51 +0000

    changed Style

--- controllers/index.js
+++ controllers/index.js
@@ -7,8 +7,8 @@ var events = require('../models/events');
 function index (request, response) {
   var now = new Date();
   var contextData = {
-    'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).',
+    'title': 'Wispy Cluster',
+    'tagline': 'The best app for managing events.',
     'events': []
   };
   for(var i=0; i<events.all.length;i++){
